4451e01811f09ee2be90bead22fbffd6564e09c5
songzhixian <3082244647@qq.com>
2026-03-06 10:58:46 +0800

    music demo branch

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main{
          * 
         */
         
-        MusicBox mbox2 = new MusicBox("A1B2C3",'N',"Sunny Day","Sunny Band","Sunny Vibes","Pop","Sunny Studio","Sunny Records");
+        MusicBox mbox2 = new MusicBox("AAA111",'N',"A Sunny Day","Sunny Band","Sunny Vibes","Pop Music","Sunny Studio","Sunny Records");
 
         playsong.playSong(mbox2.getSongID(), mbox2.getPremiumSong(),1);
 
